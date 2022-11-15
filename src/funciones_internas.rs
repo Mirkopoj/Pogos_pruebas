918a43ba603d94d02ff27f2366e40c380a667834
Mirkopoj <mirkopoj@hotmail.com>
2022-11-15 11:01:56 -0300

    plantilla lista

---
+++ src/funciones_internas.rs
@@ -0,0 +1,30 @@
+use std::sync::mpsc::{Receiver, TryRecvError};
+
+use crate::{to_bytes, TestData, TESTDATALEN};
+
+pub trait ConvertTest {
+    fn to_bytes(&self) -> [u8;TESTDATALEN];
+}
+
+impl ConvertTest for TestData {
+    fn to_bytes(&self) -> [u8;TESTDATALEN] {
+       to_bytes(*self) 
+    }
+}
+
+pub fn ver_estado (rx: &Receiver<bool>) {
+    match rx.try_recv() {
+        Ok(msg) => {
+            if msg { esperar(rx); }
+        },
+        Err(why) => {
+            if why != TryRecvError::Empty { 
+                panic!("Perdimos la pausa para las pruebas");
+            }
+        }
+    }
+}
+
+fn esperar (rx: &Receiver<bool>) {
+    while rx.recv().expect("Perdimos la pausa para las prueba, esperando") { }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,60 @@
+use std::sync::mpsc::{Receiver, Sender, SendError};
+
 use macroquad::prelude::*;
 
-pub const  TESTDATALEN: usize = 0;
+mod funciones_internas;
+use funciones_internas::*;
 
 #[derive(Copy, Clone, PartialEq, Default)]
 pub struct TestData {
+    /* Atributos que sean necesarios para comunicar los resultados de las pruebas */
 }
 
-pub fn from_bytes(_bytes_in: &[u8]) -> TestData {
-    Default::default()
-}
-
+/* Editar el valor de la constante de acuerdo al tamaño del array de bytes */
+pub const  TESTDATALEN: usize = 0;
 fn to_bytes(_struct_in: TestData) -> [u8;TESTDATALEN]{
+    /* Funcion que permita condensar los datos en un array de bytes */
     [0;TESTDATALEN]
 }
 
-pub trait ConvertTest {
-    fn to_bytes(&self) -> [u8;TESTDATALEN];
+pub fn from_bytes(_bytes_in: &[u8]) -> TestData {
+    /* Funcion inversa a to_bytes */
+    Default::default()
 }
 
-impl ConvertTest for TestData {
-    fn to_bytes(&self) -> [u8;TESTDATALEN] {
-       to_bytes(*self) 
-    }
-}
-
-pub fn prueba () {
-}
-
-pub fn resultados () {
+pub fn resultados (_test_data: TestData) {
     clear_background(GREEN);
+    /* Aqui va un hmi custom para las pruebas, utilizando macroquad */
+    /* El espacio de pantalla disponible va de (20.0, 20.0) a (230.0, 230.0) */
 }
+
+/* Editar el valor de la constante de acuerdo al numero de pruebas */
+const  NUMERODEPRUEBAS: usize = 3;
+pub fn prueba (pruebas_tx: &Sender<TestData>, pruebas_pausa_rx: &Receiver<bool>) -> Result<bool, SendError<TestData>> {
+    let mut test_data: TestData = Default::default();
+    for i in 0..NUMERODEPRUEBAS {
+        ver_estado(pruebas_pausa_rx);
+        match i {
+            /* Dentro de este bloque llamar a las pruebas que se desen en orden */
+            0 => {
+                test0(&mut test_data);
+            },
+            1 => {
+                test1();
+            },
+            2 => {
+                test2();
+            },
+            _ => { },
+        };
+
+        pruebas_tx.send(test_data)?
+    }
+
+    Ok(true) //true para placas funcionales, false para placas defectuosas
+}
+
+/* Pruebas custom */
+fn test0(_test_data: &mut TestData) {}
+fn test1() {}
+fn test2() {}
