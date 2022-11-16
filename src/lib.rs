f5b75e6e97c40b30859ad6c77c5aedaf67b52875
Mirkopoj <mirkopoj@hotmail.com>
2022-11-16 17:48:42 -0300

    Preeliminar Pruebas

--- src/lib.rs
+++ src/lib.rs
@@ -5,56 +5,261 @@ use macroquad::prelude::*;
 mod funciones_internas;
 use funciones_internas::*;
 
+use std::process::{Command, Stdio};
+use std::io::{Write, Read};
+use std::str;
+use std::time::Duration;
+use std::thread::sleep;
+use gpiod::{Chip, Options, Lines, Output, Input};
+extern crate i2c_linux;
+use i2c_linux::I2c;
+use std::fs::File;
+
 #[derive(Copy, Clone, PartialEq, Default)]
 pub struct TestData {
-    /* Atributos que sean necesarios para comunicar los resultados de las pruebas */
+    programdo_test: bool,
+    verificado_test: bool,
+    abc: u8,
+    z: u8,
+    z_cont: u8,
+    y: u8,
+    y_cont: u8,
+    tension1: f64,
+    tension2: f64,
+    tension3: f64,
+    programdo_bueno: bool,
+    verificado_bueno: bool,
 }
 
 /* Editar el valor de la constante de acuerdo al tamaño del array de bytes */
-pub const  TESTDATALEN: usize = 0;
-fn to_bytes(_struct_in: TestData) -> [u8;TESTDATALEN]{
-    /* Funcion que permita condensar los datos en un array de bytes */
-    [0;TESTDATALEN]
+pub const  TESTDATALEN: usize = 30;
+fn to_bytes(struct_in: TestData) -> [u8;TESTDATALEN]{
+    let mut ret = [0;TESTDATALEN];
+
+    if struct_in.programdo_test  { ret[0] |= 1; }
+    if struct_in.verificado_test { ret[0] |= 2; }
+    if struct_in.programdo_bueno { ret[0] |= 3; }
+    if struct_in.verificado_bueno{ ret[0] |= 4; }
+
+    ret[1] = struct_in.abc;
+    ret[2] = struct_in.z;
+    ret[3] = struct_in.z_cont;
+    ret[4] = struct_in.y;
+    ret[5] = struct_in.y_cont;
+
+    ret[06..14].copy_from_slice(&struct_in.tension1.to_be_bytes());
+    ret[14..22].copy_from_slice(&struct_in.tension1.to_be_bytes());
+    ret[22..30].copy_from_slice(&struct_in.tension1.to_be_bytes());
+
+    ret
 }
 
-pub fn from_bytes(_bytes_in: &[u8]) -> TestData {
-    /* Funcion inversa a to_bytes */
-    Default::default()
+pub fn from_bytes(bytes_in: &[u8]) -> TestData {
+    TestData { 
+        programdo_test: bytes_in[0]&1 == 1, 
+        verificado_test: bytes_in[0]&2 == 2, 
+        abc:    bytes_in[1], 
+        z:      bytes_in[2],
+        z_cont: bytes_in[3],
+        y:      bytes_in[4],
+        y_cont: bytes_in[5],
+        tension1: f64::from_be_bytes(bytes_in[06..14].try_into().expect("Slice tension1 de largo incorrecto")),
+        tension2: f64::from_be_bytes(bytes_in[14..22].try_into().expect("Slice tension1 de largo incorrecto")),
+        tension3: f64::from_be_bytes(bytes_in[22..30].try_into().expect("Slice tension1 de largo incorrecto")),
+        programdo_bueno: bytes_in[0]&3 == 3, 
+        verificado_bueno: bytes_in[0]&4 == 4, 
+    }
 }
 
 pub fn resultados (_test_data: TestData) {
     clear_background(GREEN);
-    /* Aqui va un hmi custom para las pruebas, utilizando macroquad */
-    /* El espacio de pantalla disponible va de (20.0, 20.0) a (230.0, 230.0) */
 }
 
-/* Editar el valor de la constante de acuerdo al numero de pruebas */
-const  NUMERODEPRUEBAS: usize = 3;
+const  NUMERODEPRUEBAS: usize = 1018;
 pub fn prueba (pruebas_tx: &Sender<TestData>, pruebas_pausa_rx: &Receiver<bool>) -> Result<bool, SendError<TestData>> {
     let mut test_data: TestData = Default::default();
-    for i in 0..NUMERODEPRUEBAS {
+    let mut ret = true;
+
+    let chipo = Chip::new("gpiochip2").expect("No se abrió el chip, abc"); // open chip
+    let opts = Options::output([18,17,15]) 
+        .consumer("my-outputs, abc"); 
+    let outputs = chipo.request_lines(opts).expect("Pedido de salidas rechazado, abc");
+
+    let chipi = Chip::new("gpiochip3").expect("No se abrió el chip, zy"); // open chip
+    let ipts = Options::input([4,6]) 
+        .consumer("my-inputs, zy"); 
+    let inputs = chipi.request_lines(ipts).expect("Pedido de salidas rechazado, abc");
+
+    let mut i2c = I2c::from_path("/dev/i2c-0").expect("i2c");
+    i2c.smbus_set_slave_address(0x08, false).expect("addr");
+    i2c.smbus_write_byte(42).expect("Write");//Sincronizar el pic
+    let mut tensiones: [f64;3] = [0.0;3];
+
+    for i in 0..=NUMERODEPRUEBAS {
         ver_estado(pruebas_pausa_rx);
         match i {
-            /* Dentro de este bloque llamar a las pruebas que se desen en orden */
             0 => {
-                test0(&mut test_data);
+                ret&=programar_y_verificar_test(&mut test_data);
             },
-            1 => {
-                test1();
+            1..=16 if i%2 == 0 => {
+                let abc = (i/2) as u8;
+                abc_put(abc, &outputs);
+                sleep(Duration::from_millis(1));
+                ret&=get_z(&mut test_data, &inputs, abc);
             },
-            2 => {
-                test2();
+            1..=16 if i%2 == 1 => {
+                let abc = ((i-1)/2) as u8;
+                ret&=get_y(&mut test_data, &inputs, abc);
+            },
+            17..=1017 => {
+                adc(&mut test_data, i-17, &mut tensiones, &mut i2c);
+            },
+            1082 => {
+                ret&=programar_y_verificar_bueno(&mut test_data);
             },
             _ => { },
         };
 
+        ret &= (test_data.tension1<1.7) & (test_data.tension1>1.6);
+        ret &= test_data.tension2<0.2;
+        ret &= test_data.tension3>3.2;
+
         pruebas_tx.send(test_data)?
     }
 
-    Ok(true) //true para placas funcionales, false para placas defectuosas
+    Ok(ret) //true para placas funcionales, false para placas defectuosas
 }
 
 /* Pruebas custom */
-fn test0(_test_data: &mut TestData) {}
-fn test1() {}
-fn test2() {}
+fn programar_y_verificar_test(struct_in: &mut TestData) -> bool {
+    struct_in.verificado_test = programar_y_verificar("/home/dietpi/TestCode.hex"); 
+    struct_in.programdo_test = true;
+    struct_in.verificado_test
+}
+
+fn programar_y_verificar_bueno(struct_in: &mut TestData) -> bool {
+    struct_in.verificado_bueno = programar_y_verificar("/home/dietpi/FinalCode.hex"); 
+    struct_in.programdo_bueno = true;
+    struct_in.verificado_bueno
+}
+
+fn get_z(struct_in: &mut TestData, inputs: &Lines<Input>, conv: u8) -> bool {
+    let z_medido = inputs.get_values([Some(false),None]).expect("No se leyó z")[1].expect("No había x");
+
+    let a = (conv>>0 & 1) == 1; 
+    let b = (conv>>1 & 1) == 1; 
+    let c = (conv>>2 & 1) == 1; 
+
+    let z_calculado = (a^b)&(b|c);
+    
+    let z = if z_medido { 1 } else { 0 };
+    struct_in.z |= z<<struct_in.z_cont;
+    struct_in.z_cont += 1;
+
+    z_calculado == z_medido
+}
+
+fn get_y(struct_in: &mut TestData, inputs: &Lines<Input>, conv: u8) -> bool {
+    let y_medido = inputs.get_values([None,Some(false)]).expect("No se leyó y")[1].expect("No había x");
+
+    let a = (conv>>0 & 1) == 1; 
+    let b = (conv>>1 & 1) == 1; 
+    let c = (conv>>2 & 1) == 1; 
+
+    let y_calculado = (a^b)&(b|c);
+    
+    let y = if y_medido { 1 } else { 0 };
+    struct_in.y |= y<<struct_in.y_cont;
+    struct_in.y_cont += 1;
+
+    y_calculado == y_medido
+}
+
+fn adc(struct_in: &mut TestData,
+       cont: usize,
+       tension_tot: &mut [f64;3],
+       i2c: &mut I2c<File>,
+       ){
+
+    for i in 0..3 {
+        let adch = i2c.smbus_read_byte().expect("ReadH") as u16;
+        let adcl = i2c.smbus_read_byte().expect("ReadL") as u16;
+        let adc:u16 = (adch <<8) + adcl;
+        let tension = adc as f64 * 0.003225806452;
+        tension_tot[i] += tension;
+    }
+
+    struct_in.tension1 = tension_tot[0]/cont as f64;
+    struct_in.tension2 = tension_tot[1]/cont as f64;
+    struct_in.tension3 = tension_tot[2]/cont as f64;
+}
+
+/* Auxiliares de pruebas */
+fn programar_y_verificar(path_to_hex: &'static str) -> bool {
+    let mut ret = true;
+
+    let id = Command::new("p16")
+        .arg("id")
+        .output()
+        .expect("Falló pickle id");
+
+    let idstr = String::from_utf8(id.stdout).expect("No se puede convertir id");
+
+    ret &= if idstr.contains("pic16_read_config_memory"){
+        false
+    } else {
+        true
+    };
+
+    let mut clear = Command::new("p16")
+        .arg("blank")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Falló pickle blank");
+
+    clear.stdin.take().expect("No se abrió el stdin").write(b"y").expect("No se escribió");
+    let mut respuesta: [u8;50] = [0;50];
+    clear.stdout.expect("No se abrió el stdout").read(&mut respuesta).expect("Falló leer stdout");
+    let clrstr = str::from_utf8(&respuesta).expect("No se puede convertir program");
+
+    ret &= if clrstr.contains("pic16_read_config_memory"){
+        false
+    } else {
+        true
+    };
+
+    let program = Command::new("p16")
+        .arg("program")
+        .arg(path_to_hex)
+        .output()
+        .expect("Falló pickle program");
+
+    let prgstr = String::from_utf8(program.stdout).expect("No se puede convertir program");
+
+    ret &= if prgstr.contains("pic16_read_config_memory"){
+        false
+    } else {
+        true
+    };
+
+    let verify = Command::new("p16")
+        .arg("verify")
+        .arg(path_to_hex)
+        .output()
+        .expect("Falló pickle verify");
+
+    let verstr = String::from_utf8(verify.stdout).expect("No se puede convertir verify");
+
+    ret &= if verstr.contains("Fail: 0"){
+        true
+    } else {
+        false
+    };
+
+    ret
+}
+
+fn abc_put(abc: u8, outputs: &Lines<Output>){
+    outputs.set_values(abc).expect("No se seteó abc");
+}
