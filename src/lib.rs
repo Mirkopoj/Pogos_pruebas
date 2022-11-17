9bb1de81a2da03d98e3c2a5e3fd712930c62069c
Mirkopoj <mirkopoj@hotmail.com>
2022-11-17 15:41:24 -0300

    Mejorando HMI 32

--- src/lib.rs
+++ src/lib.rs
@@ -79,7 +79,10 @@ pub fn resultados (test_data: TestData) {
     let font_size = 40.0;
 
     draw_rectangle(x+8.0, y-10.0, 142.0, (y_step*11.0)+1.0, LIGHTGRAY);
-    draw_rectangle(x+8.0, y+(y_step*2.1)+2.0, 72.0, (y_step*1.0)+1.0, GREEN);
+    for rect in 0..test_data.abc {
+        let offset = rect as f32;
+        draw_rectangle(x+8.0, y+(y_step*2.1)+2.0+offset*y_step, 72.0, (y_step*1.0)+1.0, GREEN);
+    }
 
     draw_text("+---+-+-+", x, y, font_size, BLACK);
     draw_text("|ABC|Z|Y|", x, y+y_step, font_size, BLACK);
